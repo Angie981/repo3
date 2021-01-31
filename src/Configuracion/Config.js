d10bc8de5b14cef136f7a54ac1ba597c622867be
Angie981 <angie.palacios@unmsm.edu.pe>
2021-01-30 19:13:53 -0500

    aa

--- src/Configuracion/Config.js
+++ src/Configuracion/Config.js
@@ -1,4 +1,5 @@
 
-const CONFIG = 'http://' + window.location.hostname + ':8081/'
-//const CONFIG= 'https://sigapdev2-consultarecibos-back.herokuapp.com/'   
+//const CONFIG = 'http://' + window.location.hostname + ':8081/'
+//const CONFIG= 'https://sigapdev2-consultarecibos-back.herokuapp.com/'  
+const CONFIG='https://sigap-consultarecibos-back-sm.herokuapp.com/'  
 export default CONFIG;
